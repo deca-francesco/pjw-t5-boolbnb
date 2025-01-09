ddaa2e7c0ee595f4e915cf428a351e3e1bedd7a1
Pietroo03 <pietro.ponte.03@gmail.com>
2025-01-09 15:27:21 +0100

    add routes connection on app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const server = express();
 const BnbRouter = require('./routes/BnbRoutes')
 const cors = require('cors')
 
+server.use(cors());
+
 const HOST = process.env.HOST
 const PORT = process.env.PORT
 
@@ -10,4 +12,9 @@ server.listen(PORT, () => {
     console.log(`Server is listening on ${HOST}:${PORT}`);
 })
 
-server.use(cors());
+server.get('/', (req, res) => {
+    res.send('Server is up and running')
+})
+
+// BnB Routes
+server.use('/appartamenti', BnbRouter)
