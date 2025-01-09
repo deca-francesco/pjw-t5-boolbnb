21c82d68ac2009e4e3f3cb1c19f91ab26eee557c
Pietroo03 <pietro.ponte.03@gmail.com>
2025-01-09 15:52:21 +0100

    fix bug on app.js add changes to comments

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const server = express();
 const BnbRouter = require('./routes/BnbRoutes')
 const cors = require('cors')
 const NotFound = require('./middleware/NotFound')
-const ServerErrorsHandler = require('./middlewares/ServerErrorsHandler')
+const ServerErrorsHandler = require('./middleware/ServerErrorsHandler')
 
 server.use(cors());
+
+server.use(express.json());
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT
@@ -17,13 +19,12 @@ server.listen(PORT, () => {
 server.get('/', (req, res) => {
     res.send('Server is up and running')
 })
-server.use(express.json());
 
 // BnB Routes
 server.use('/appartamenti', BnbRouter)
 
-// Gestione errore 404
+// Handle 404 errors
 server.use(NotFound)
 
-// gestire gli errori generali del server (500)
+// Handle 500 errors
 server.use(ServerErrorsHandler)
