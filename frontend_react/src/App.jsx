b1087b507e8ac3f674894434a860431603e49748
FrancescoMarra97 <francescomarra192@gmail.com>
2025-01-13 16:33:22 +0100

    Route AppLayout

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AppLayout from './layout/AppLayuot'
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
 
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
