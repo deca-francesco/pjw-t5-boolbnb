9b129d31f67bb846605aaedfd03cd6e544050c7b
FrancescoMarra97 <francescomarra192@gmail.com>
2025-01-13 16:47:28 +0100

    Add route HomePage

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppLayout from './layout/AppLayuot'
+import HomePage from './pages/HomePage'
 function App() {
 
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route element={<AppLayout />} />
+          <Route path='/' element={<HomePage />} />
 
-          </Route>
         </Routes>
       </BrowserRouter>
     </>
