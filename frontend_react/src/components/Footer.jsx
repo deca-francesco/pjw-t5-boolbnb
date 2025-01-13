b325829636aabab08a14bc1fca7148a13f29d9c9
FrancescoMarra97 <francescomarra192@gmail.com>
2025-01-13 17:13:53 +0100

    Added footer

--- frontend_react/src/components/Footer.jsx
+++ frontend_react/src/components/Footer.jsx
@@ -2,8 +2,20 @@ export default function Footer() {
 
     return (
         <>
-            <footer>
+            <footer className="bg-dark py-5 text-white">
+                <div className="container">
+                    <div className="row">
+                        <div className="about">
 
+                        </div>
+                        <div className="social d-flex">
+                            <i className="m-2 bi bi-facebook"></i>
+                            <i className="m-2 bi bi-twitter"></i>
+                            <i className="m-2 bi bi-instagram"></i>
+                        </div>
+
+                    </div>
+                </div>
             </footer>
         </>
     )
