26c257db6646bfd17213984697e7bb4c8911fa24
FrancescoMarra97 <francescomarra192@gmail.com>
2025-01-14 10:36:29 +0100

    Footer and Header

--- frontend_react/src/components/Footer.jsx
+++ frontend_react/src/components/Footer.jsx
@@ -2,16 +2,39 @@ export default function Footer() {
 
     return (
         <>
-            <footer className="bg-dark py-5 text-white">
+            <footer className="bg-dark py-5 text-white d-flex justify-content-between">
                 <div className="container">
                     <div className="row">
-                        <div className="about">
-
+                        <div className="about col-md">
+                            <nav>
+                                <h5>ABOUT US</h5>
+                                <ul className="list-unstyled">
+                                    <li> <a href="/" className="text-white text-decoration-none"> <strong>Chi siamo</strong> </a> </li>
+                                    <li> <a href="/" className="text-white text-decoration-none"> <strong>Privacy Policy</strong></a> </li>
+                                </ul>
+                            </nav>
                         </div>
-                        <div className="social d-flex">
-                            <i className="m-2 bi bi-facebook"></i>
-                            <i className="m-2 bi bi-twitter"></i>
-                            <i className="m-2 bi bi-instagram"></i>
+                        <div className="contact-info col-md">
+                            <h5>CONTACT US</h5>
+                            <p><i className="bi bi-geo-alt"></i> 1234 Main Street, Cityville, Country</p>
+                            <p><i className="bi bi-envelope"></i> contact@example.com</p>
+                            <p><i className="bi bi-phone"></i> +1 234 567 890</p>
+                        </div>
+
+
+                        <div className="social col-md">
+                            <h5>FOLLOW US</h5>
+                            <ul className="list-unstyled">
+                                <li className="mb-2">
+                                    <a className="text-decoration-none text-white" href="/"><i className="m-2 bi bi-facebook"></i> <strong>Facebook</strong> </a>
+                                </li>
+                                <li className="mb-2">
+                                    <a className="text-decoration-none text-white" href="/"><i className="m-2 bi bi-twitter"></i> <strong>Twitter</strong> </a>
+                                </li>
+                                <li className="mb-2">
+                                    <a className="text-decoration-none text-white" href="/"><i className="m-2 bi bi-instagram"></i> <strong>Instagram</strong></a>
+                                </li>
+                            </ul>
                         </div>
 
                     </div>
--- frontend_react/src/components/Header.jsx
+++ frontend_react/src/components/Header.jsx
@@ -2,8 +2,20 @@ export default function Header() {
 
     return (
         <>
-            <header>
+            <header className="bg-dark py-3 px-4 shadow ">
+                <div className="container d-flex justify-content-between align-items-center">
+                    <div className="logo">
 
+                    </div>
+
+                    <nav className="nav">
+                        <ul className="d-flex list-unstyled m-0">
+                            <li className="mx-3">
+                                <a href="/" className="text-white text-decoration-none">Home</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </header>
         </>
     )
