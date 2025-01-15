4b6742ad0eca5b290624d65189ae703815a87e4a
giorgiadifranco <giorgiadf08@gmail.com>
2025-01-15 12:23:25 +0100

    fix images

--- frontend_react/src/components/ApartmentsComponents/SingleApartment.jsx
+++ frontend_react/src/components/ApartmentsComponents/SingleApartment.jsx
@@ -1,29 +1,91 @@
 import React from 'react';
+import HeartIconVote from './HeartIconVote'
 
-const ApartmentCard = ({ apartment }) => {
+const ApartmentCard = ({ apartment, setApartments }) => {
     return (
         <div>
-            <div>
+            <div className='m-4'>
                 <h2>{apartment.title}</h2>
             </div>
-            <div>
-                {/* Check if the image exists and display it if true */}
-                {apartment.image && (
-                    <div className="apartment-image mb-3">
-                        <img
-                            src={apartment.image}
-                            alt={apartment.title}
-                            className="img-fluid"
-                            style={{ maxWidth: "100%", height: "auto" }}
-                        />
-                    </div>
-                )}
-                {/* Display the apartment details */}
-                <p><strong>Beds:</strong> {apartment.beds}</p>
-                <p><strong>Bathrooms:</strong> {apartment.bathrooms}</p>
-                <p><strong>Square Meters:</strong> {apartment.square_meters} m²</p>
-                <p><strong>Rooms:</strong> {apartment.rooms}</p>
-                <p><strong>Address:</strong> {apartment.address}</p>
+
+            <div className='mt-3 mb-3'>
+            <div className="card mb-3 border-0 shadow">
+    <div className="row g-0">
+      {/* Colonna per l'immagine grande - SISTEMARE BG CARD */}
+      {apartment.image && (
+        <div className="col-6 p-1">
+          <img
+            src={apartment.image}
+            alt={apartment.title}
+            className="img-fluid rounded"
+            style={{ height: "100%", objectFit: "cover", width: "100%" }}
+          />
+        </div>
+      )}
+
+      {/* Colonna per le immagini piccole */}
+      <div className="col-6 d-flex flex-wrap">
+        {apartment.image && (
+          <>
+            {/* Quattro immagini piccole */}
+            <div className="col-6 p-1">
+              <img
+                src={apartment.image}
+                alt={`${apartment.title} Small 1`}
+                className="img-fluid rounded"
+                style={{ objectFit: "cover", width: "100%", height: "auto" }}
+              />
+            </div>
+            <div className="col-6 p-1">
+              <img
+                src={apartment.image}
+                alt={`${apartment.title} Small 2`}
+                className="img-fluid rounded"
+                style={{ objectFit: "cover", width: "100%", height: "auto" }}
+              />
+            </div>
+            <div className="col-6 p-1">
+              <img
+                src={apartment.image}
+                alt={`${apartment.title} Small 3`}
+                className="img-fluid rounded"
+                style={{ objectFit: "cover", width: "100%", height: "auto" }}
+              />
+            </div>
+            <div className="col-6 p-1">
+              <img
+                src={apartment.image}
+                alt={`${apartment.title} Small 4`}
+                className="img-fluid rounded"
+                style={{ objectFit: "cover", width: "100%", height: "auto" }}
+              />
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  </div>
+                
+
+                       
+                    
+                    
+                     {/* Display the apartment details*/}
+                     {/*<div className='col-4'>
+                            <p className='mb-1'><strong>Beds:</strong> {apartment.beds}</p>
+                            <p className='mb-1'><strong>Bathrooms:</strong> {apartment.bathrooms}</p>
+                            <p className='mb-1'><strong>Square Meters:</strong> {apartment.square_meters} m²</p>
+                            <p className='mb-1'><strong>Rooms:</strong> {apartment.rooms}</p>
+                            <p className='mb-1'><strong>Address:</strong> {apartment.address}</p>
+                        </div>
+
+                        <div className='col-2'>
+                            <HeartIconVote data_apartment={apartment} setApartments={setApartments} />
+                        </div>*/}
+                
+                {/*fino a qui affiancare immagine con dati appartamenti */}
+
+
 
                 {/* Display the services if available */}
                 {apartment.services && apartment.services.length > 0 && (
