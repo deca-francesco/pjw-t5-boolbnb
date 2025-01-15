e4d03a5a5ad9bae77d368e7e2647c5dc0fcb3e64
Pietroo03 <pietro.ponte.03@gmail.com>
2025-01-15 12:16:04 +0100

    bug fixed

--- frontend_react/src/pages/ProtectedHomePage.jsx
+++ frontend_react/src/pages/ProtectedHomePage.jsx
@@ -1,19 +1,19 @@
 import ApartmentCard from '../components/ApartmentsComponents/ApartmentCard';
 import { useState, useEffect } from 'react'
 
+
 export default function ProtectedHomePage() {
+
 
     // url api
     const base_api_url = import.meta.env.VITE_EXPRESS_API_SERVER
     const apartment_api_url = base_api_url + '/apartments'
     const [apartments, setApartments] = useState([]);
 
-    console.log(apartment_api_url);
-
-
     useEffect(() => {
 
         //make a fetch request to the base api endpoint
+
         fetch(apartment_api_url)
             .then(res => {
                 if (!res.ok) {
@@ -22,7 +22,7 @@ export default function ProtectedHomePage() {
                 return res.json();
             })
             .then(data => {
-                console.log(data);
+                console.log(data.data);
                 setApartments(data.data);
 
             }).catch(err => console.log(err))
@@ -33,18 +33,24 @@ export default function ProtectedHomePage() {
         <>
             <section className='d-flex justify-content-around'>
                 <div className='container'>
-                    <div className='row row-cols-3 p-5 g-5 mt-0'>
+                    <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 p-5 g-5 mt-0'>
+
+
 
                         {
-                            apartments && apartments.map(apartment => (<div className='col' key={apartment.id}><ApartmentCard apartment={apartment} /> </div>))
+                            apartments && apartments.map(apartment => (<div className='col' key={apartment.id}><ApartmentCard apartment={apartment} setApartments={setApartments} /> </div>))
                         }
+
 
                     </div>
                 </div>
 
+
             </section>
 
         </>
+
+
 
     )
 }
